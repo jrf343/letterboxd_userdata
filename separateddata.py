be4385d74aaf2248cea32f0ec01194d704a22ed6
Julia Frances <104040854+jrf343@users.noreply.github.com>
2024-01-29 10:48:22 -0500

    updated json files

--- separateddata.py
+++ separateddata.py
@@ -35,6 +35,9 @@ def extract_combined_data(file_path):
 
     # Convert stars in the Rating column to a numeric value
     movies_df['Rating'] = movies_df['Rating'].apply(lambda x: len(x) if isinstance(x, str) and '★' in x else (len(x) + 0.5) if isinstance(x, str) and '½' in x else x)
+    
+    # Replace NaN values in 'Review' with "null"
+    movies_df['Review'] = movies_df['Review'].apply(lambda x: None if pd.isna(x) else x)
 
     # Convert DataFrame to a list of dictionaries
     movies_list = movies_df.to_dict(orient='records')
@@ -54,6 +57,9 @@ output_movies_info_path = "output_movies_information.json"
 follow_info_list = []
 movies_info_list = []
 
+# Original user list from file names
+original_users = set(os.path.splitext(f)[0][:-5] for f in os.listdir(folder_path) if f.endswith('.csv'))
+
 # Iterate through each combined file in the folder
 for file in os.listdir(folder_path):
     if file.endswith('.csv'):
@@ -62,11 +68,15 @@ for file in os.listdir(folder_path):
         # Extract information from the combined file
         user, movies, followers, following = extract_combined_data(file_path)
 
+        # Filter followers and followees based on original_users
+        filtered_followers = [follower for follower in followers if follower in original_users]
+        filtered_following = [followee for followee in following if followee in original_users]
+
         # Add the following and follower information to the list
         follow_info_list.append({
             'User': user,
-            'Followers': followers,
-            'Following': following
+            'Followers': filtered_followers,
+            'Following': filtered_following
         })
 
         # Add the movie review data to the list
